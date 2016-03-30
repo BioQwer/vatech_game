35c74b11b012b4db383db3d696f2db7556aaeb87
Antony Alexsandrov <bioqwer94@gmail.com>
2016-03-31 00:17:55 +0300

    #1 angular views in progress welcome page in work

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,25 +1,31 @@
-var app = angular.module("app");
+(function () {
 
-app.config(['$routeProvider',
-  function ($routeProvider) {
-    $routeProvider.when('/private', {
-      templateUrl: 'templates/welcome.html',
-      controller: 'AddOrderController'
-    }).otherwise({
-      redirectTo: '/AddNewOrder'
-    });
-  }]);
+  var app = angular.module("app", ['ngRoute']);
 
-
-app.controller('AddOrderController', function ($scope) {
-
-  $scope.message = 'This is Add new order screen';
-
-});
+  app.config(['$routeProvider',
+    function ($routeProvider) {
+      $routeProvider
+        .when('private', {
+          templateUrl: 'private',
+          controller: 'WelcomeController'
+        }).otherwise({
+        templateUrl: 'templates/welcome.html',
+        controller: 'WelcomeController'
+      });
+    }]);
 
 
-app.controller('ShowOrdersController', function ($scope) {
+  app.controller('WelcomeController', function ($scope) {
 
-  $scope.message = 'This is Show orders screen';
+    $scope.message = 'This is Add new order screen';
+    console.log("sadasd");
 
-});
+  });
+
+
+  app.controller('ShowOrdersController', function ($scope) {
+
+    $scope.message = 'This is Show orders screen';
+
+  });
+})();
